1acce2eec2d4278c05faf17667fbb94edbf02778
lmgv66 <51816696+lmgv66@users.noreply.github.com>
2024-12-02 11:27:53 +0100

    Add files via upload

---
+++ config_file_auth/auth_config_4.js
@@ -0,0 +1,3 @@
+const AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.authToken1";
+const CLIENT_ID = "client12345";
+const AUTH_URL = "https://auth.example.com";
---
+++ config_file_auth/auth_config_8.js
@@ -0,0 +1,3 @@
+const AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.authToken3";
+const CLIENT_ID = "client12345";
+const AUTH_URL = "https://auth.example.com";
